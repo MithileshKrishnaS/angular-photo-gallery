11d5121a446bfe3e68a204da52ba7e64350e7ac6
MithileshKrishnaS <mithileshkrishna20@gmail.com>
2021-12-21 22:20:32 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GalleryComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
